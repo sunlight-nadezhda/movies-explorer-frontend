e5cbf5517732fd8f7e01fed4d61052987922731d
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-08-03 19:44:58 +0300

    feat: добавить компонент Promo

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Main.css';
+import Promo from "../Promo/Promo";
 
 class Main extends React.Component {
 
   render() {
     return (
-      <div className="page"></div>
+      <div className="page">
+        <Promo />
+      </div>
     );
   }
 }
