a6e7b18c59f7a054210f8b0deafdbd99859e86f5
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-08-03 21:49:50 +0300

    feat: добавить компонент AboutProject

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './AboutProject.css';
+
+class AboutProject extends React.Component {
+
+  render() {
+    return (
+      <section className="about-project">
+        <h2 className="about-project__title">О проекте</h2>
+        <div className="about-project__description">
+          <h3 className="about-project__description-tile">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <h3 className="about-project__description-tile">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className="about-project__description-info">
+            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
+          </p>
+          <p className="about-project__description-info">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
+          </p>
+        </div>
+        <div className="about-project__ratio">
+          <p className="about-project__ratio-time about-project__ratio-time_color_black">1 неделя</p>
+          <p className="about-project__ratio-time about-project__ratio-time_color_gray">4 недели</p>
+          <p className="about-project__ratio-name">Back-end</p>
+          <p className="about-project__ratio-name">Front-end</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default AboutProject;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Main.css';
 import NavTab from "../NavTab/NavTab";
 import Promo from "../Promo/Promo";
+import AboutProject from "../AboutProject/AboutProject";
 
 class Main extends React.Component {
 
@@ -10,6 +11,7 @@ class Main extends React.Component {
       <div className="page">
         <NavTab />
         <Promo />
+        <AboutProject />
       </div>
     );
   }
