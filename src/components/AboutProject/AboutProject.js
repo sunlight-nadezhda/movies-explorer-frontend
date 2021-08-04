0d080cbcee45213ffb5d071f3ad7687e44019f28
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-08-04 19:07:18 +0300

    feat: сделать страницу landing адаптивной

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -8,22 +8,26 @@ class AboutProject extends React.Component {
       <section className="section about-project">
         <h2 className="section__title">О проекте</h2>
         <div className="about-project__description">
-          <h3 className="about-project__description-tile">
-            Дипломный проект включал 5 этапов
-          </h3>
-          <h3 className="about-project__description-tile">
-            На выполнение диплома ушло 5 недель
-          </h3>
-          <p className="about-project__description-info">
-            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
-          </p>
-          <p className="about-project__description-info">
-            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
-          </p>
+          <div className="about-project__column">
+            <h3 className="about-project__description-tile">
+              Дипломный проект включал 5&nbsp;этапов
+            </h3>
+            <p className="about-project__description-info">
+              Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.
+            </p>
+          </div>
+          <div className="about-project__column">
+            <h3 className="about-project__description-tile">
+              На выполнение диплома ушло 5&nbsp;недель
+            </h3>
+            <p className="about-project__description-info">
+              У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.
+            </p>
+          </div>
         </div>
         <div className="about-project__ratio">
-          <p className="about-project__ratio-time about-project__ratio-time_color_black">1 неделя</p>
-          <p className="about-project__ratio-time about-project__ratio-time_color_gray">4 недели</p>
+          <p className="about-project__ratio-time about-project__ratio-time_color_black">1&nbsp;неделя</p>
+          <p className="about-project__ratio-time about-project__ratio-time_color_gray">4&nbsp;недели</p>
           <p className="about-project__ratio-name">Back-end</p>
           <p className="about-project__ratio-name">Front-end</p>
         </div>
