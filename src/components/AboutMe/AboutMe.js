2e695d577bd734c4a7b4f4211edf0a0f25a02f28
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-10-05 20:45:26 +0300

    feat: заменить информацию в лендинге о студенте на информацию о себе.

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './AboutMe.css';
 import SectionTitle from '../SectionTitle/SectionTitle';
-import photoPath from '../../images/photo.jpg';
+import photoPath from '../../images/myphoto.jpg';
 import arrowPath from '../../images/arrow.svg';
 
 const AboutMe = () => {
@@ -10,32 +10,30 @@ const AboutMe = () => {
       <SectionTitle title="Студент" />
       <div className="about-me__description">
         <div className="about-me__text">
-          <h3 className="about-me__name">Виталий</h3>
-          <h4 className="about-me__about">Фронтенд-разработчик, 30 лет</h4>
+          <h3 className="about-me__name">Надежда</h3>
+          <h4 className="about-me__about">Фронтенд-разработчик, 35 лет</h4>
           <p className="about-me__info">
-            Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+            Я родилась и живу в Симферополе, закончила факультет математики и информатики ТНУ им. В.И. Вернадского. У меня есть муж и сын. Я люблю слушать музыку, а ещё увлекаюсь гимнастикой. Недавно начала программировать. С 2008 года занималась анализом данных. 2020 - 2021 гг. прошла курс по веб-разработке под присмотром опытных наставников.
           </p>
           <div className="about-me__social">
-            <a href="https://www.facebook.com/" className="about-me__social-item" target="_blank"
-              rel="noopener noreferrer">Facebook</a>
-            <a href="https://github.com/" className="about-me__social-item" target="_blank"
+            <a href="http://github.com/sunlight-nadezhda" className="about-me__social-item" target="_blank"
               rel="noopener noreferrer">Github</a>
           </div>
         </div>
         <img alt='Фото студента' src={photoPath} className="about-me__image" />
       </div>
       <h5 className="about-me__portfolio-title">Портфолио</h5>
-      <a href="https://github.com/" className="about-me__portfolio-item" target="_blank"
+      <a href="https://sunlight-nadezhda.github.io/how-to-learn/index.html" className="about-me__portfolio-item" target="_blank"
         rel="noopener noreferrer">
         <div className="about-me__portfolio-name">Статичный сайт</div>
         <img alt='Стрелка' src={arrowPath} />
       </a>
-      <a href="https://github.com/" className="about-me__portfolio-item" target="_blank"
+      <a href="https://sunlight-nadezhda.github.io/russian-travel/index.html" className="about-me__portfolio-item" target="_blank"
         rel="noopener noreferrer">
         <div className="about-me__portfolio-name">Адаптивный сайт</div>
         <img alt='Стрелка' src={arrowPath} />
       </a>
-      <a href="https://github.com/" className="about-me__portfolio-item" target="_blank"
+      <a href="https://github.com/sunlight-nadezhda/react-mesto-api-full" className="about-me__portfolio-item" target="_blank"
         rel="noopener noreferrer">
         <div className="about-me__portfolio-name">Одностраничное приложение</div>
         <img alt='Стрелка' src={arrowPath} />
