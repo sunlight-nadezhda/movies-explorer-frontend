f10153ec9e01acdefc0bece3f7de0bfa077c7437
sunlight-nadezhda <75030781+sunlight-nadezhda@users.noreply.github.com>
2021-10-16 01:06:38 +0300

    feat: добавить тест проверки работы чекбокс фильтра

--- src/components/FilterCheckbox/FilterCheckbox.test.js
+++ src/components/FilterCheckbox/FilterCheckbox.test.js
@@ -18,9 +18,31 @@ afterEach(() => {
   container = null;
 });
 
-it("renders with or without a name", () => {
+it("renders text", () => {
   act(() => {
-    render(<FilterCheckbox />, container);
+    const onChange = jest.fn();
+    render(<FilterCheckbox onChange={onChange} value={false} />, container);
   });
   expect(container.textContent).toBe("Короткометражки");
+});
+
+it("changes value when clicked", () => {
+  const onChange = jest.fn();
+  const value = false;
+  act(() => {
+    render(<FilterCheckbox onChange={onChange} value={value} />, container);
+  });
+
+  // получаем элемент toggle и кликаем на него
+  const toggle = container.querySelector(".filter__input");
+  const filter = container.querySelector(".filter__label");
+
+  expect(toggle.checked).toBeFalsy();
+
+  act(() => {
+    filter.dispatchEvent(new MouseEvent("click", { bubbles: true, cancelable: true, button: 0, composed: true }));
+  });
+
+  expect(onChange).toHaveBeenCalledTimes(1);
+  expect(onChange).toHaveBeenCalledWith(!value);
 });
